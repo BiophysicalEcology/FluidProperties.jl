0cfd2cd8a23472ef636cc8d399230fa468548e26
mrke <mrke@unimelb.edu.au>
2025-10-27 14:46:48 +1100

    fix default rh to be fractional in wet_air_properties

--- src/fluid_properties.jl
+++ src/fluid_properties.jl
@@ -78,7 +78,7 @@ either (1) psychrometric data (T_wetbulb or rh), or (2) hygrometric data (T_dew)
 
 """
 wet_air_properties(::Missing, ::Missing, ::Missing; kwargs...) = missing
-@inline function wet_air_properties(T_drybulb, rh=100.0, P_atmos=101325u"Pa"; 
+@inline function wet_air_properties(T_drybulb, rh=1.0, P_atmos=101325u"Pa"; 
     fO2=0.2095,
     fCO2=0.0004,
     fN2=0.79,
