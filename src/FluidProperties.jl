8ea6745dac0d9f633a937cd5da84a25650edf4ca
Rafael Schouten <rafaelschouten@gmail.com>
2026-01-13 15:13:25 +1100

    return structs and improve variable names

--- src/FluidProperties.jl
+++ src/FluidProperties.jl
@@ -12,7 +12,7 @@ const atm = uconvert(u"Pa", 1Unitful.atm)
 
 export atmospheric_pressure, dry_air_properties, enthalpy_of_vaporisation, molar_enthalpy_of_vaporisation
 export vapour_pressure, water_properties, wet_air_properties
-export GasFractions
+export GasFractions, DryAirProperties, WetAirProperties, WaterProperties
 
 @compound H2O
 @compound O2
